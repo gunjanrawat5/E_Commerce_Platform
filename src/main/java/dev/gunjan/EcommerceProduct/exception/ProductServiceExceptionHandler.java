907e46aff11023efc3133777ee531a3a62ca7251
Gunjan <gunjanrawat5@gmail.com>
2026-04-25 20:30:44 -0400

    feat: added exception handler

---
+++ src/main/java/dev/gunjan/EcommerceProduct/exception/ProductServiceExceptionHandler.java
@@ -0,0 +1,17 @@
+package dev.gunjan.EcommerceProduct.exception;
+
+import dev.gunjan.EcommerceProduct.dto.ExceptionResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ProductServiceExceptionHandler {
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity handleProductNotFoundException(ProductNotFoundException pe){
+        ExceptionResponseDTO exceptionResponseDTO = new ExceptionResponseDTO(pe.getMessage(), 404);
+        return new ResponseEntity<>(exceptionResponseDTO, HttpStatus.NOT_FOUND);
+    }
+}
